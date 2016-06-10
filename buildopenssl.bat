45d5947dab47952ba0023c01b89e1d2ef549d13a
Joshua Boyd <jdboyd@jdboyd.net>
2016-06-10 19:26:32 +0000

    Some openssl work.

--- buildopenssl.bat
+++ buildopenssl.bat
@@ -1,11 +1,11 @@
 set BUILD_ROOT=c:\Qt5.5.0\
 
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64_x86
 
-cd openssl
+cd openssl-1.0.2h
 set CL=/MP
-perl Configure VC-WIN64A --prefix=%BUILD_ROOT%\openssl\build
-call ms\do_win64a
+perl Configure VC-WIN32 no-asm --prefix=%BUILD_ROOT%\openssl\build
+call ms\do_nasm
 nmake -f ms\ntdll.mak
 nmake -f ms\ntdll.mak install
 cd ..
